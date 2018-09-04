03a35dc9c19481e9340faade01824b0858fc41b2
Sh1kari <bubasev13@gmail.com>
2018-09-04 21:47:20 +0300

    add github info, post actions and reducers

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -11,11 +11,17 @@ import Spinner from '../common/Spinner';
 import { getProfileByHandle } from '../../actions/profileActions';
 
 class Profile extends Component {
-  componentDidMount = () => {
+  componentDidMount() {
     if (this.props.match.params.handle) {
       this.props.getProfileByHandle(this.props.match.params.handle);
     }
-  };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.profile.profile === null && this.props.profile.loading) {
+      this.props.history.push('/not-found');
+    }
+  }
 
   render() {
     const { profile, loading } = this.props.profile;
@@ -36,9 +42,14 @@ class Profile extends Component {
           </div>
 
           <ProfileHeader profile={profile} />
-          <ProfileAbout />
-          <ProfileCreds />
-          <ProfileGithub />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          {profile.githubusername ? (
+            <ProfileGithub username={profile.githubusername} />
+          ) : null}
         </div>
       );
     }
